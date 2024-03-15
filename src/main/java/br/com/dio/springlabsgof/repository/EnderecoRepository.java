d6e130b66c1679cc7d83b57b969784999944b0d8
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2024-03-15 17:51:48 -0300

    fix: alter JpaRepository<Endereco, Long> to JpaRepository<Endereco, String> in EnderecoRepository

--- src/main/java/br/com/dio/springlabsgof/repository/EnderecoRepository.java
+++ src/main/java/br/com/dio/springlabsgof/repository/EnderecoRepository.java
@@ -3,6 +3,6 @@ package br.com.dio.springlabsgof.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.dio.springlabsgof.model.Endereco;
 
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+public interface EnderecoRepository extends JpaRepository<Endereco, String> {
 
 }

56a1c8f50b999bf993b2ec5c0f1013874597d1aa
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2024-03-15 10:27:41 -0300

    primeiro commit após fazer o spring initializer

---
+++ src/main/java/br/com/dio/springlabsgof/SpringlabsPadroesProjetoApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.springlabsgof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlabsPadroesProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlabsPadroesProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dio/springlabsgof/SpringlabsPadroesProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.springlabsgof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlabsPadroesProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

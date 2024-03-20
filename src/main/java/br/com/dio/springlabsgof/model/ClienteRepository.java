f42becf455d08b2dadf30c757ce5ac1dba985834
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2024-03-19 23:11:32 -0300

    feat: adding ClienteRepository and EnderecoRepository into model package

---
+++ src/main/java/br/com/dio/springlabsgof/model/ClienteRepository.java
@@ -0,0 +1,10 @@
+package br.com.dio.springlabsgof.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+}
